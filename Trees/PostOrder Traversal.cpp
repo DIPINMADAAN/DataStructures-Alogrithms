37fcb52d464fbcacc0325d7f77f8643fb9a0a5e4
DIPIN MADAAN <dipin.madaan14@gmail.com>
2020-01-20 04:53:32 +0530

    Trees complete codes

--- Trees/PostOrder Traversal.cpp
+++ Trees/PostOrder Traversal.cpp
@@ -1,5 +1,38 @@
-/* you only have to complete the function given below.  
-Node is defined as  
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+/*
 
 class Node {
     public:
@@ -26,3 +59,24 @@ class Node {
         cout<<root->data<<" ";
 
     }
+}; //End of Solution
+
+int main() {
+  
+    Solution myTree;
+    Node* root = NULL;
+    
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+  
+	myTree.postOrder(root);
+    return 0;
+}
+
--- Trees/PreOrder Traversal.cpp
+++ Trees/PreOrder Traversal.cpp
@@ -1,7 +1,38 @@
-/* you only have to complete the function given below.  
-Node is defined as  
+#include <bits/stdc++.h>
+
+using namespace std;
 
 class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+/*class Node {
     public:
         int data;
         Node *left;
@@ -24,3 +55,24 @@ class Node {
         preOrder(root->left);
         preOrder(root->right);
     }
+}; //End of Solution
+
+int main() {
+  
+    Solution myTree;
+    Node* root = NULL;
+    
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+  
+	myTree.preOrder(root);
+    return 0;
+}
+
